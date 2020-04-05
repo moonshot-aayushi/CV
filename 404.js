92a093747c742850d3a3289f9eef75019eb402e8
Aayushi Iyer <51299602+moonshot-aayushi@users.noreply.github.com>
2020-04-06 04:04:40 +0530

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
+import Layout from './Layout';
+import Container from './Container';
 
 const NotFoundPage = () => {
   return (
